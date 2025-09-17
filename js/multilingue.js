f175ad9c4fe9eab47e9fe368a307333c30e6a942
ryan95380 <ryanmambou270@gmail.com>
2025-09-17 17:06:48 +0200

    ryan

---
+++ js/multilingue.js
@@ -0,0 +1,14 @@
+let btnfr = document.querySelector("language-switch");
+let btnen = document.querySelector("language-switch")
+
+
+// async function translations(langue) {
+
+//     try{
+//         const reponse = await  fetch ("translation.json");
+//         if (reponse.ok)  } else {
+//             throw new Error('erreur'); 
+//         }
+//     }
+
+
